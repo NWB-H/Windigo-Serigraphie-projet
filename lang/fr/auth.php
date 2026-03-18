b29dc8800ae4f335b0eb89c7cf032f69e645c876
acoubard <alexis.coubard@cdigitallabs.com>
2026-03-18 16:46:50 +0100

    feat: add translation for error message

---
+++ lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de langue d'authentification
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langue suivantes sont utilisées durant l'authentification
+    | pour différents messages à afficher à l'utilisateur. Vous pouvez les
+    | modifier selon les besoins de votre application.
+    |
+    */
+
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+
+];
---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de langue de réinitialisation du mot de passe
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langue suivantes sont les messages par défaut correspondant
+    | aux raisons retournées par le gestionnaire de mots de passe lors d'une
+    | tentative de mise à jour (ex. mot de passe ou jeton invalide).
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous avons envoyé le lien de réinitialisation de votre mot de passe par e-mail.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne trouvons aucun utilisateur avec cette adresse e-mail.",
+
+];
