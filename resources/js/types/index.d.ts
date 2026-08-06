520b249a7aa3fa690d49b9cece6d93ae735aa24b
kmeleoonn <couby72220@gmail.com>
2026-08-06 21:04:26 +0200

    feat: add modal on admin layout

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,6 +1,6 @@
-import { Address } from '@/models/Address';
 import { InertiaLinkProps } from '@inertiajs/vue3';
 import type { LucideIcon } from 'lucide-vue-next';
+import {User} from "@/models/User";
 
 export interface Auth {
     user: User | null;
@@ -28,17 +28,6 @@ export type AppPageProps<
     enabled: boolean;
 };
 
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    avatar?: string;
-    email_verified_at: string | null;
-    created_at: string;
-    updated_at: string;
-    last_login_at: Date;
-    role: string;
-    addresses: Address[];
-}
+
 
 export type BreadcrumbItemType = BreadcrumbItem;
